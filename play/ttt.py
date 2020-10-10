dda4a987f27a9b5f3e36bc33bfbefcfe561944a4
Nitehawk <nite97m@gmail.com>
2020-10-10 18:40:42 -0400

    Get guesses

--- play/ttt.py
+++ play/ttt.py
@@ -9,7 +9,7 @@
 # Design elements
 #   Board state tracked as array of characters. Index 0 is ignored for display simplicity
 
-def display_board(board = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], legend=True):
+def display_board(board = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], legend=False):
     """ Prints out the current board state.   Default board is filled with 'X' player for validation """
     # Sanity check that the board is proper length
     if len(board) != 10:
@@ -33,15 +33,35 @@ def new_board():
 
 def get_play(board, player):
     """ Get a square selection from the player and validate it
-        player is a tupple (num, symbol)
-        return the selected square index
+        return the selected square index - return 0 for a skipped turn
         """
-    pass
+    for tries in range(2,-1,-1):   # We'll give the player 3 attempts to make a valid selection
+        sel = input(f"Player {player}: Select your move: ")
+        try:
+            sel = int(sel)
+        except:
+            print(f"That was not a valid move, try again.  {tries} remaining")
+            continue
+        if 0 < sel < 10:  # Make sure the selection is in range
+            if board[sel] == " ":
+                return sel
+            else:
+                print(f"That square is already occupied. Try again.  {tries} remaining")
+                if tries > 0: display_board(board, True)
+                continue
+        else:
+            print(f"Input must be between 1 and 9.  Try again. {tries} remaining")
+            if tries > 0: display_board(board, True)
+    return 0  # Fall through if the player doesn't make a valid selection
 
 def game_loop():
     board = new_board()
     display_board(board, False)
+    selection = get_play(board, 1)
+    board[selection] = 'X'
+    display_board(board, False)
 
+"""
 playing=True
 while playing:
     playing = False # This forces the game to exit unless the play again select y or yes
@@ -52,4 +72,5 @@ while playing:
     if again.lower() == 'y' or again.lower() == 'yes':
         playing = True
     print('')
-        
+"""        
+game_loop()
