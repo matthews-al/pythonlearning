d3cd6a01cffd40fa4013b835e17e3902ef9d69c3
Matthew Schlegel <mschlegel@alertlogic.com>
2020-10-19 10:03:50 -0400

    Make things lint better.

--- play/ttt.py
+++ play/ttt.py
@@ -1,21 +1,23 @@
-# Section 7 - Milestone project 1
-# Requirements:
-#   Display board state
-#   Select play from numpad grid
-#   Two local players
-#   First player selects symbol
-#   Detect game state (win / tie)
+"""
+Section 7 - Milestone project 1
+Requirements:
+  Display board state
+  Select play from numpad grid
+  Two local players
+  First player selects symbol
+  Detect game state (win / tie)
 
-# Design elements
-#   Board state tracked as array of characters. Index 0 is ignored for display simplicity
+Design elements
+  Board state tracked as array of characters. Index 0 is ignored for display simplicity
+"""
 
-def display_board(board = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], legend=False):
-    """ Prints out the current board state.   Default board is filled with 'X' player for validation """
+def display_board(board, legend=False):
+    """ Prints out the current board state. """
     # Sanity check that the board is proper length
     if len(board) != 10:
         print("corrupt board")
-        exit
-    
+        return
+
     # Loop through the board to print state
     for i in range(7, 0, -3):
         if legend:
@@ -35,7 +37,7 @@ def get_play(board, player):
     """ Get a square selection from the player and validate it
         return the selected square index - return 0 for a skipped turn
         """
-    for tries in range(2,-1,-1):   # We'll give the player 3 attempts to make a valid selection
+    for tries in range(2, -1, -1):   # We'll give the player 3 attempts to make a valid selection
         sel = input(f"Player {player}: Select your move: ")
         try:
             sel = int(sel)
@@ -45,10 +47,9 @@ def get_play(board, player):
         if 0 < sel < 10:  # Make sure the selection is in range
             if board[sel] == " ":
                 return sel
-            else:
-                print(f"That square is already occupied. Try again.  {tries} remaining")
-                if tries > 0: display_board(board, True)
-                continue
+            print(f"That square is already occupied. Try again.  {tries} remaining")
+            if tries > 0: display_board(board, True)
+            continue
         else:
             print(f"Input must be between 1 and 9.  Try again. {tries} remaining")
             if tries > 0: display_board(board, True)
@@ -56,7 +57,7 @@ def get_play(board, player):
 
 def get_pone():
     """ Ask player one to select their symbol """
-    for tries in range(2,-1,-1):   # We'll give the player 3 attempts to make a valid selection
+    for tries in range(2, -1, -1):   # We'll give the player 3 attempts to make a valid selection
         sym = input("Player 1, please choose your symbol - 'X' or 'O': ")
         if sym.upper() == 'X' or sym.upper() == 'O':
             return sym.upper()
@@ -66,9 +67,11 @@ def get_pone():
 
 def check_wins(board):
     """ Check the given board to see if there's a winner.
-    returns winning symbol if there's a winner, ' ' if there's no winner, or 't' if the game has completed in a tie.
-    Note:  I'm sure there are some clever ways to loop through the rows and columns, vs. hard coding the sets,
-    however, for this scale, it would likely be overly complex
+    returns winning symbol if there's a winner,
+     ' ' if there's no winner,
+     't' if the game has completed in a tie.
+    Note:  I'm sure there are some clever ways to loop through the rows and columns,
+     vs. hard coding the sets, however, for this scale, it would likely be overly complex
     """
     # Check rows:
     if board[7] != " " and board[7] == board[8] == board[9]: return board[7]
@@ -104,14 +107,15 @@ def game_loop():
     while playing:
         display_board(board)
         selection = get_play(board, curplayer+1)
-        if selection > 0:  board[selection] = players[curplayer]   # Do this so we can rely on board[0] being ' '
+        # Do this so we can rely on board[0] being ' '
+        if selection > 0: board[selection] = players[curplayer]
         curplayer = (curplayer + 1) % 2
         status = check_wins(board)
         if status == 't':
             print('The game has tied.')
             display_board(board)
             return
-        if status == 'X' or status == 'O':
+        if status in ('X', 'O'):
             display_board(board)
             print(f"The {status}'s have won!   Congrats!")
             return
@@ -121,7 +125,7 @@ def game_loop():
 
     display_board(board)
 
-playing=True
+playing = True
 while playing:
     playing = False # This forces the game to exit unless the play again select y or yes
     game_loop()
